0c00cd19f4ff212f2de8c025e28fc38d50ec4663
Smaran-Teja-P <smaranteja32@gmail.com>
2024-05-15 23:23:33 -0400

    added stacks:  alignment and spacing, spacers

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(alignment: .center, spacing: 30) {
+            Text("1").font(.title3)
+            Text("2").font(.caption)
+            Text("3").font(.largeTitle)
         }
-        .padding()
+        
+        HStack(alignment: .center, spacing: 30) {
+            Text("1").font(.title3)
+            Text("2").font(.caption)
+            Text("3").font(.largeTitle)
+        }
+        
+        HStack(alignment: .center, spacing: 30) {
+            Text("1").font(.title3)
+            Text("2").font(.caption)
+            Text("3").font(.largeTitle)
+        }
+        
+        Spacer()
+            .frame(height: 100.0)
+        
+        ZStack{
+            Text("Does this give you a")
+            Text("SEIZURE?").font(.title)
+        }
     }
 }
 
