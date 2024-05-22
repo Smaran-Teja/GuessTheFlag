6bdc03b7acfeb8c050e7ad4e1d6edcef5feec026
Smaran-Teja-P <smaranteja32@gmail.com>
2024-05-21 22:21:53 -0400

    added background color

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,30 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .center, spacing: 30) {
-            Text("1").font(.title3)
-            Text("2").font(.caption)
-            Text("3").font(.largeTitle)
-        }
-        
-        HStack(alignment: .center, spacing: 30) {
-            Text("1").font(.title3)
-            Text("2").font(.caption)
-            Text("3").font(.largeTitle)
-        }
-        
-        HStack(alignment: .center, spacing: 30) {
-            Text("1").font(.title3)
-            Text("2").font(.caption)
-            Text("3").font(.largeTitle)
-        }
-        
-        Spacer()
-            .frame(height: 100.0)
-        
-        ZStack(alignment: .top){
-            Text("Does this give you a")
-            Text("SEIZURE?").font(.title)
+        ZStack(){
+            Color.purple.ignoresSafeArea()
+            Text("\"Hello world!\"")
         }
     }
 }
