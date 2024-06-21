7d72291b0abbe7ea39e53c7f16cec129e097bb03
Smaran-Teja-P <smaranteja32@gmail.com>
2024-06-20 22:37:13 -0400

    Added alert dialog

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,19 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     var body: some View {
         ZStack{
             LinearGradient(colors: [.red, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             Button() {
+                showingAlert = true
                 print("Hello World")
             }
             label: {
                 Label("Edit", systemImage: "pencil")
             }
             .buttonStyle(.borderedProminent)
-            .tint(.purple)
-            
+            .tint(.red)
+            .alert("Editing content", isPresented: $showingAlert) {
+                Button("Stop", role: .destructive) { }
+                Button("Yes", role: .cancel) { }
+            }
+            message: {
+                Text("This action will overwrite data, are you sure you want to proceed?")
+            }
         }
     }
 }
