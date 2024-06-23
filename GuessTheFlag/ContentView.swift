d60b77f30239b965120a9cec94c915f1d136a9d4
Smaran-Teja-P <smaranteja32@gmail.com>
2024-06-22 23:39:30 -0400

    Included resetting, showing alert, and basic game functionality

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
     var body: some View {
         ZStack {
@@ -23,13 +26,32 @@ struct ContentView: View {
                 
                 ForEach(0..<3) { number in
                     Button {
-                        print ("Flag is tapped")
+                        onFlagPress(number)
                     } label: {
                         Image(countries[number])
                     }
                 }
             }
+        }.alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: resetQuestion)
+        } message: {
+            Text("Your score is ???")
         }
+    }
+    func onFlagPress(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct Answer!"
+        }
+        else {
+            scoreTitle = "Oops, wrong answer..."
+        }
+        
+        showingScore = true
+    }
+    
+    func resetQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
