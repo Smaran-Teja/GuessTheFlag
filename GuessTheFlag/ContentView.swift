6ccebbbe208c844742013c9e8eb693138bff2c32
Smaran-Teja-P <smaranteja32@gmail.com>
2024-06-27 12:44:17 -0400

    Design changes

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,18 +10,23 @@ import SwiftUI
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-    
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
     var body: some View {
         ZStack {
-            Color.blue.ignoresSafeArea()
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
             VStack (spacing: 30) {
                 VStack{
-                    Text("Tap the flag of").foregroundStyle(.white)
-                    Text(countries[correctAnswer]).font(.largeTitle).foregroundStyle(.white
-                    )
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                        .fontWeight(.light)
+                    
+                    Text(countries[correctAnswer])
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
                 }
                 
                 ForEach(0..<3) { number in
@@ -29,7 +34,9 @@ struct ContentView: View {
                         onFlagPress(number)
                     } label: {
                         Image(countries[number])
-                    }
+                            .clipShape(.capsule)
+                            .shadow(radius: 10)
+                    } 
                 }
             }
         }.alert(scoreTitle, isPresented: $showingScore) {
